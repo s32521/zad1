030f8b7f02367ef886a60befd51cc271101040c1
s32521 <s32521@pjwstk.edu.pl>
2025-03-11 14:15:44 +0100

    Update task2.java

--- untitled/src/task2.java
+++ untitled/src/task2.java
@@ -1,7 +1,19 @@
-
+import java.util.Scanner;
 public class task2 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(n<0){
+            System.out.println("The number is negative");
+            return;
+        }else if(n>0){
+            System.out.println("The number is positive");
+            return;
+        }else{
+            System.out.println("The number is zero");
+            return;
+        }
         
     }
 }
